76fef6ae1374f04f1763416e0b2cd12fa8c7b098
sofiane <s.msatfa@ecole-ipssi.net>
2024-05-03 13:11:25 +0200

    ajout(front-technique): ajout material ui

---
+++ apps/web/src/contexts/settings/settings-context.ts
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import type { PaletteMode } from "@mui/material";
+
+export type ThemeColorPresets =
+  | "default"
+  | "cyan"
+  | "purple"
+  | "blue"
+  | "orange"
+  | "red";
+
+export interface SettingsValue {
+  themeMode: PaletteMode;
+  themeColorPresets: ThemeColorPresets;
+}
+
+export interface SettingsContextType extends SettingsValue {
+  setThemeMode: (mode: PaletteMode) => void;
+  toggleThemeMode: () => void;
+  setThemeColorPresets: (color: ThemeColorPresets) => void;
+}
+
+export const SettingsContext = createContext<SettingsContextType | undefined>(
+  undefined,
+);
---
+++ apps/web/src/hooks/use-local-storage.ts
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    setStoredValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue] as const;
+};
