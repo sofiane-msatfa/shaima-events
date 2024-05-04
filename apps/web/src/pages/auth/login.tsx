9321e985415950d391383f617ce324455502863f
Amano <97117336+AymaneIsmail@users.noreply.github.com>
2024-05-04 16:34:31 +0200

    add style (#14)
    * add style
    
    * fix review
    
    ---------
    
    Co-authored-by: Aymane <aymane@cashflowpositifi.fr>
    Co-authored-by: sofiane <s.msatfa@ecole-ipssi.net>
    Co-authored-by: Sofiane MSATFA <147051593+sofiane-msatfa@users.noreply.github.com>

--- apps/web/src/pages/auth/login.tsx
+++ apps/web/src/pages/auth/login.tsx
@@ -3,6 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { loginRequestSchema, type LoginRequest } from "@common/dto/login-request";
 import { useAuth } from "@/contexts/auth/use-auth";
 import { useRouter } from "@/hooks/use-router";
+import { Avatar, Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
 
 Component.displayName = "LoginPage";
 
@@ -30,12 +31,51 @@ export function Component() {
   });
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" placeholder="Email" {...register("email", { required: true })} />
-      <input type="password" placeholder="Password" {...register("password", { required: true })} />
-      <button type="submit">Login</button>
-      {errors.email && <p>{errors.email.message}</p>}
-      {errors.password && <p>{errors.password.message}</p>}
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Typography component="h1" variant="h5" marginTop={2} marginBottom={2}>
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                placeholder="Email"
+                {...register("email", { required: true })}
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+              />
+              {errors.email && <p>{errors.email.message}</p>}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+              />
+              {errors.password && <p>{errors.password.message}</p>}
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
--- apps/web/src/pages/auth/register.tsx
+++ apps/web/src/pages/auth/register.tsx
@@ -4,6 +4,18 @@ import { registerRequestSchema, type RegisterRequest } from "@common/dto/registe
 import { useAuth } from "@/contexts/auth/use-auth";
 import { useRouter } from "@/hooks/use-router";
 import { z } from "zod";
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 Component.displayName = "RegisterPage";
 
@@ -48,20 +60,99 @@ export function Component() {
   });
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" placeholder="Firstname" {...register("firstname", { required: true })} />
-      <input type="text" placeholder="Lastname" {...register("lastname", { required: true })} />
-      <input type="text" placeholder="Email" {...register("email", { required: true })} />
-      <input type="password" placeholder="Password" {...register("password", { required: true })} />
-      <input
-        type="password"
-        placeholder="Confirm password"
-        {...register("passwordConfirmation", { required: true })}
-      />
-      <button type="submit">Register</button>
-      {errors.email && <p>{errors.email.message}</p>}
-      {errors.password && <p>{errors.password.message}</p>}
-      {errors.passwordConfirmation && <p>{errors.passwordConfirmation.message}</p>}
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Typography component="h1" variant="h5" marginTop={2} marginBottom={2}>
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="text"
+                placeholder="Firstname"
+                {...register("firstname", { required: true })}
+                fullWidth
+                id="firstname"
+                label="Firstname"
+                name="firstname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="text"
+                placeholder="Lastname"
+                {...register("lastname", { required: true })}
+                fullWidth
+                id="lastname"
+                label="Lastname"
+                name="lastname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                placeholder="Email"
+                {...register("email", { required: true })}
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+              />
+              {errors.email && <p>{errors.email.message}</p>}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+              />
+              {errors.password && <p>{errors.password.message}</p>}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                placeholder="Confirm password"
+                {...register("passwordConfirmation", { required: true })}
+                fullWidth
+                id="passwordConfirmation"
+                label="Confirm password"
+                name="passwordConfirmation"
+              />
+              {errors.passwordConfirmation && <p>{errors.passwordConfirmation.message}</p>}
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link to="#">Forgot password ?</Link>
+            </Grid>
+            <Grid item>
+              <Link to="#">{"Don't have an account ? Sign Up"}</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
